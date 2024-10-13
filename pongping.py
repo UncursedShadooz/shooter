584c5e29ce5b9509b00359f8148613b08aa99243
UncursedShadooz <143077743+UncursedShadooz@users.noreply.github.com>
2024-10-13 10:21:32 +0700

    Create pongping.py

---
+++ pongping.py
@@ -0,0 +1,41 @@
+from pygame import *
+from random import randint
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if key[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+class Bot(GameSprite):
+    def auto_play(self, ball_y):
+        difference = ball_y - self.rect.y 
+        if difference < 0:
+            self.rect.y -= randint(2, 4)
+        else:
+            self.rect.y += randint(2, 4)
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
