1668cd4b6f1e3f42e19729f6acceabe887102f94
UncursedShadooz <143077743+UncursedShadooz@users.noreply.github.com>
2024-10-27 10:20:56 +0700

    Update pongping.py

--- pongping.py
+++ pongping.py
@@ -1,3 +1,4 @@
+
 from pygame import *
 from random import randint
 
@@ -23,7 +24,7 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if key[K_s] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
 class Bot(GameSprite):
@@ -39,3 +40,61 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+
+game = True
+finish = False
+
+playerLeft = Player("wall.png", 10, 250, 5, 25, 100)
+playerRight = Player("wall.png", 565, 250, 5, 25, 100)
+ball = Player("ball.png", 300, 250, 5, 50, 50)
+
+font.init()
+font1 = font.Font(None, 70)
+text_1 = font1.render("LEFT VICTORY!", True, (255,0,0))
+text_2 = font1.render("RIGHT VICTORY!", True, (255,0,0))
+
+FPS = 50
+clock = time.Clock()
+
+ball_speed_x = ball.speed
+ball_speed_y = ball.speed
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if not finish:
+        window.fill((200, 255, 255))
+
+        ball.rect.x += ball_speed_x
+        ball.rect.y += ball_speed_y
+        
+        if sprite.collide_rect(ball, playerLeft):
+            ball_speed_x = ball.speed
+
+        if sprite.collide_rect(ball, playerRight):
+            ball_speed_x = -ball.speed
+
+        if ball.rect.y > win_height - 50:
+            ball_speed_y = -ball.speed
+        
+        if ball.rect.y < 0:
+            ball_speed_y = ball.speed
+
+        if ball.rect.x < 0:
+            window.blit(text_1, (win_width/5, win_height/2))
+            finish = True
+
+        if ball.rect.x > win_width - 50:
+            window.blit(text_2, (win_width/5, win_height/2))
+            finish = True
+
+        playerLeft.update_l()
+        playerRight.update_r()
+
+        playerLeft.reset()
+        playerRight.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
